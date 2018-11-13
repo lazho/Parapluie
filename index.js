98e11322854fa8501a0298d6e6c3c72686de478d
lazho <lazho@protonmail.com>
2018-11-12 21:59:24 -0500

    fix indent

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const { prefix } = require('./config.json');
 const { token } = require('./secret.json');
 
 client.once('ready', () => {
-    console.log('Parapluie is ready!');
+	console.log('Parapluie is ready!');
 });
 
 client.on('message', message => {
-    if (message.content.startsWith(`${prefix}beep`)) {
-        message.channel.send('`boop`');
-    }
+	if (message.content.startsWith(`${prefix}beep`)) {
+		message.channel.send('`boop`');
+	}
 });
 
 client.login(token);
